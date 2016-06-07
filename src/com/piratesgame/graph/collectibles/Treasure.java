a0eae443c64fd3508336a6f4740f17150e8f9878
lewispettifer <lewispettifer10@students.tretherras.net>
2016-06-07 15:02:09 +0100

    Addresses issue #3

---
+++ src/com/piratesgame/graph/collectibles/Treasure.java
@@ -0,0 +1,5 @@
+package com.piratesgame.graph.collectibles;
+
+public class Treasure extends Collectible {
+
+}
---
+++ src/com/piratesgame/graph/collectibles/treasure/Gold.java
@@ -0,0 +1,8 @@
+package com.piratesgame.graph.collectibles.treasure;
+import com.piratesgame.graph.collectibles.Treasure;
+
+class Gold extends Treasure {
+    public Gold(){
+        this.setLimit(20);
+    }
+}
