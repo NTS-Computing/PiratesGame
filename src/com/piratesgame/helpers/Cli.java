01606be898c11c63eda61cf42ce86200e5b2c8ad
George stallard <Georgestallard@students.tretherras.net>
2016-06-16 12:16:00 +0100

    Cli needs move method

--- src/com/piratesgame/helpers/Cli.java
+++ src/com/piratesgame/helpers/Cli.java
@@ -3,29 +3,34 @@ package com.piratesgame.helpers;
 import com.piratesgame.graph.Edge;
 import com.piratesgame.graph.Vertex;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cli {
     int numberOfMoves = 0;
-    public static String getUserInput(String msg){
-        System.out.println(msg + ": ");
+    public static void movingboat(){
 
-        Scanner in = new Scanner(System.in);
-        String userInput = in.nextLine();
-        in.close();
-
-        return userInput;
     }
 
-    public static int getUserInputInt(String msg){
-        return Integer.parseInt(getUserInput(msg));
+    public static int getUserChoiceForMovment(){
+        Scanner Scan = new Scanner(System.in);
+        System.out.println ("Enter your choice, using a single interger.");
+        return Integer.parseInt(Scan.nextLine());
+
     }
-    public static void getOptionsForMovement(Vertex v){
+    public static Edge getOptionsForMovementandchoice(Vertex v){
         ArrayList<Edge> destination = v.getNeighbourhood();
-        for(Edge e : destination){
-     //       System.out.println(e.getNeighbour(v).getLabel());
+        int loopnumber = 0;
+        int IndexNumber = 0;
+        while (destination.size() >= loopnumber) {
+            String Destin = IndexNumber + ":" + destination.get(loopnumber);
+            System.out.println(Destin);
+            loopnumber = loopnumber + 1;
         }
+            int choice = getUserChoiceForMovment();
+            Edge des = destination.get(choice -1);
+            return des;
 
     }
 }
